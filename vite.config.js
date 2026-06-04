3f12774cc2c0081b755ed668e6fb6cf225e67c19
NzhRmr <nazhromar@gmail.com>
2026-06-04 18:16:38 +0800

    fix: use correct vite react plugin

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/react-swc' // (or '@vitejs/plugin-react' depending on your setup)
+import react from '@vitejs/plugin-react' // 👈 Swapped this to the standard plugin
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Multi-Quiz-App/', // 👈 ADD THIS LINE
+  base: '/Multi-Quiz-App/', 
 })
