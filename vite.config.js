891c0a1261d6c444faabfde824664d0096ff1d08
NazhRomar <nazhromar@yahoo.com.ph>
2026-09-03 03:25:59 +0800

    Revert empty plugins array, no longer needed
    Was only added for Cloudflare's wrangler auto-setup step, which we've
    moved away from in favor of Vercel.

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   base: '/', // Change this back to '/' (or delete the base line entirely)
-  plugins: [],
   define: {
     __BUILD_DATE__: JSON.stringify(new Date().toISOString()),
   },
